422d184446b70e980e998a58b0fd8b55ec9d71ad
alonlevi <alon.levi3@mail.huji.ac.il>
2024-11-18 15:36:59 +0200

    3split

--- FastSimCluster.py
+++ FastSimCluster.py
@@ -15,17 +15,20 @@ config_name = sys.argv[3]
 
 with open(config_dir+"/inner"+config_name, 'r') as inner_file:  #
     cfg_inner = json.loads(inner_file.read())
-with open(config_dir+"/outer"+config_name, 'r') as outer_file:
-    cfg_outer = json.loads(outer_file.read())
+with open(config_dir+"/outer_1"+config_name, 'r') as outer_file:
+    cfg_outer_1 = json.loads(outer_file.read())
+with open(config_dir+"/outer_2"+config_name, 'r') as outer_file:
+    cfg_outer_2 = json.loads(outer_file.read())
 
 cfg_inner['outputDir'] = sys.argv[2]
-cfg_outer['outputDir'] = sys.argv[2]
-
+cfg_outer_1['outputDir'] = sys.argv[2]
+cfg_outer_2['outputDir'] = sys.argv[2]
 
 numEpochs = int(sys.argv[1])
 
 cfg_inner["numEpochs"] = numEpochs
-cfg_outer["numEpochs"] = numEpochs
+cfg_outer_1["numEpochs"] = numEpochs
+cfg_outer_2["numEpochs"] = numEpochs
 
 #LOCAL TESTING
 # cfg_inner["data_path"] = 'TrainData/neutron_inner_1M.csv'
@@ -34,15 +37,17 @@ cfg_outer["numEpochs"] = numEpochs
 
 create_time = time.localtime()
 
-print("Creating outer trainer...")
-outer_trainer = create_trainer(cfg_outer)
+print("Creating outer trainers...")
+outer_trainer_1 = create_trainer(cfg_outer_1)
+outer_trainer_2 = create_trainer(cfg_outer_2)
 print("Creating inner trainer...")
 inner_trainer = create_trainer(cfg_inner)
 
 training_time = time.localtime()
 print(f"Trainers Created ! Time elapsed : {utils.get_time(training_time, create_time)} \nStarting outer Training:")
 
-outer_trainer.run()
+outer_trainer_1.run()
+outer_trainer_2.run()
 outer_time = time.localtime()
 print(f"Outer Training Done! Time elapsed : {utils.get_time(outer_time, training_time)} \nStarting inner Training:")
 inner_trainer.run()
@@ -50,21 +55,28 @@ inner_time = time.localtime()
 print(f"Inner Training Done! Time elapsed : {utils.get_time(inner_time, outer_time)} \nMaking dataframes:")
 
 KL_in = np.zeros(len(inner_trainer.KL_Div))
-KL_out = np.zeros(len(outer_trainer.KL_Div))
+KL_out_1 = np.zeros(len(outer_trainer_1.KL_Div))
+KL_out_2 = np.zeros(len(outer_trainer_2.KL_Div))
 
 for i in range(len(KL_in)):
     KL_in[i] = inner_trainer.KL_Div[i]
-for i in range(len(KL_out)):
-    KL_out[i] = outer_trainer.KL_Div[i]
-
+for i in range(len(KL_out_1)):
+    KL_out_1[i] = outer_trainer_1.KL_Div[i]
+for i in range(len(KL_out_2)):
+    KL_out_2[i] = outer_trainer_2.KL_Div[i]
 
 np.save(inner_trainer.outputDir+'KL_in.npy', KL_in)
-np.save(outer_trainer.outputDir+'KL_out.npy', KL_out)
+np.save(outer_trainer_1.outputDir+'KL_out.npy', KL_out_1)
+np.save(outer_trainer_2.outputDir+'KL_out.npy', KL_out_2)
 np.save(inner_trainer.outputDir+'D_losses_in.npy', inner_trainer.D_Losses)
-np.save(outer_trainer.outputDir+'D_losses_out.npy', outer_trainer.D_Losses)
+np.save(outer_trainer_1.outputDir+'D_losses_out.npy', outer_trainer_1.D_Losses)
+np.save(outer_trainer_2.outputDir+'D_losses_out.npy', outer_trainer_2.D_Losses)
 np.save(inner_trainer.outputDir+'Val_D_losses_in.npy', inner_trainer.Val_D_Losses)
-np.save(outer_trainer.outputDir+'Val_D_losses_out.npy', outer_trainer.Val_D_Losses)
+np.save(outer_trainer_1.outputDir+'Val_D_losses_out.npy', outer_trainer_1.Val_D_Losses)
+np.save(outer_trainer_2.outputDir+'Val_D_losses_out.npy', outer_trainer_2.Val_D_Losses)
 np.save(inner_trainer.outputDir+'G_losses_in.npy', inner_trainer.G_Losses)
-np.save(outer_trainer.outputDir+'G_losses_out.npy', outer_trainer.G_Losses)
+np.save(outer_trainer_1.outputDir+'G_losses_out.npy', outer_trainer_1.G_Losses)
+np.save(outer_trainer_2.outputDir+'G_losses_out.npy', outer_trainer_2.G_Losses)
 np.save(inner_trainer.outputDir+'Val_G_losses_in.npy', inner_trainer.Val_G_Losses)
-np.save(outer_trainer.outputDir+'Val_G_losses_out.npy', outer_trainer.Val_G_Losses)
+np.save(outer_trainer_1.outputDir+'Val_G_losses_out.npy', outer_trainer_1.Val_G_Losses)
+np.save(outer_trainer_2.outputDir+'Val_G_losses_out.npy', outer_trainer_2.Val_G_Losses)
