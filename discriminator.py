93e2fcc77d6454748dbd5d66ac30c3795966c102
alonlevi <alon.levi3@mail.huji.ac.il>
2025-04-29 15:21:54 +0300

    disc2 fix

--- discriminator.py
+++ discriminator.py
@@ -64,27 +64,38 @@ class Discriminator(nn.Module):
 class Discriminator2(nn.Module):
     def __init__(self, numFeatures):
         super().__init__()
-        self.main = nn.Sequential(
-            nn.Linear(numFeatures, 256, bias=True),
-            nn.InstanceNorm1d(256, affine=True),
-            nn.LeakyReLU(0.2),
+        self.linear1 = nn.Linear(numFeatures, 256, bias=True)
+        self.norm1 = nn.InstanceNorm1d(256, affine=True)
 
-            nn.Linear(256, 512, bias=True),
-            nn.InstanceNorm1d(512, affine=True),
-            nn.LeakyReLU(0.2),
+        self.linear2 = nn.Linear(256, 512, bias=True)
+        self.norm2 = nn.InstanceNorm1d(512, affine=True)
 
-            nn.Linear(512, 256, bias=True),
-            nn.InstanceNorm1d(256, affine=True),
-            nn.LeakyReLU(0.2),
+        self.linear3 = nn.Linear(512, 256, bias=True)
+        self.norm3 = nn.InstanceNorm1d(256, affine=True)
 
-            nn.Linear(256, 1, bias=False)
-        )
+        self.linear4 = nn.Linear(256, 1, bias=False)
+        self.act = nn.LeakyReLU(0.2)
 
     def forward(self, x):
-        x = x.unsqueeze(2)  # (B, D) → (B, D, 1)
-        x = self.main(x)
-        return x.squeeze(2)
+        x = self.linear1(x)
+        x = x.unsqueeze(2)
+        x = self.norm1(x).squeeze(2)
+        x = self.act(x)
+
+        x = self.linear2(x)
+        x = x.unsqueeze(2)
+        x = self.norm2(x).squeeze(2)
+        x = self.act(x)
+
+        x = self.linear3(x)
+        x = x.unsqueeze(2)
+        x = self.norm3(x).squeeze(2)
+        x = self.act(x)
+
+        x = self.linear4(x)
+        return x
 
     def get_param_number(self):
         return sum(p.numel() for p in self.parameters())
+
 
