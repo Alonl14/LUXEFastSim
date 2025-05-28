3d1a7de707698b98ac1fb8f6d02f4b4015d4eab9
alonlevi <alon.levi3@mail.huji.ac.il>
2025-05-28 16:01:11 +0300

    noiseDim tries

--- trainer.py
+++ trainer.py
@@ -66,7 +66,7 @@ class Trainer:
         self.KL_Div = np.array([])
         self.real_buffer = []
         self.fake_buffer = []
-        self.KL_sample_target = 8192
+        self.KL_sample_target = 16384
 
     def run(self):
         print("Starting Training Loop...")
