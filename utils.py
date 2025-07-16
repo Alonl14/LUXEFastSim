72e17d8d4bcc0c4ebecb225168994843328ea2a4
alonlevi <alon.levi3@mail.huji.ac.il>
2025-07-16 14:17:48 +0300

    factor 100

--- utils.py
+++ utils.py
@@ -307,6 +307,7 @@ def generate_ds(generator_net, factor, cfg):
     :param cfg:
     :return: numpy array of generated data
     """
+    print(f"Starting to work on dataset for region {cfg['dataGroup']}...")
     ds = dataset.ParticleDataset(cfg)
     print(f"Created dataset for region {cfg['dataGroup']} with {len(ds.data)} events")
     numEvents = np.shape(ds.data)[0]
@@ -362,7 +363,7 @@ def generate_fake_real_dfs(run_id, cfg, run_dir, generator_net=None):
 
     # TODO: remove factor, find a different way to ease local data generation
     # Read data used for training
-    fake_df, real_df = generate_ds(generator_net, factor=2, cfg=cfg)
+    fake_df, real_df = generate_ds(generator_net, factor=100, cfg=cfg)
     real_df = real_df[real_df[' time'] <= 1e6]
     fake_df = fake_df[fake_df[' time'] <= 1e6]
     add_features(fake_df, cfg['pdg'])
